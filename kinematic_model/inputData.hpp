0f9dcbb44987eb6e12e676f38628a945a3518c5d
Keval Shah <kevalshah278@gmail.com>
2022-05-30 15:36:20 -0400

    added parameters in csv & corrected indentation

--- kinematic_model/inputData.hpp
+++ kinematic_model/inputData.hpp
@@ -17,77 +17,77 @@ class Input
 
     public:
 
-    std::string readFileIntoString(const std::string& path) 
-    {
-        auto ss = std::ostringstream{};
-        std::ifstream input_file(path);
-        ss << input_file.rdbuf();
-        return ss.str();
-    }
+        std::string readFileIntoString(const std::string& path) 
+        {
+            auto ss = std::ostringstream{};
+            std::ifstream input_file(path);
+            ss << input_file.rdbuf();
+            return ss.str();
+        }
 
-    
-    std::map<int, std::vector<std::string>> getFileContent()
-    {
-        std::string filename("inputData.csv");
-        std::string file_contents = readFileIntoString(filename);;
-        char delimiter{','};
+        
+        std::map<int, std::vector<std::string>> getFileContent()
+        {
+            std::string filename("inputData.csv");
+            std::string file_contents = readFileIntoString(filename);;
+            char delimiter{','};
 
-        std::istringstream sstream(file_contents);
-        std::vector<std::string> items;
-        std::string record;
+            std::istringstream sstream(file_contents);
+            std::vector<std::string> items;
+            std::string record;
 
-        int counter{0};
-        while(std::getline(sstream, record)) 
-            {
-                std::istringstream line(record);
-                while(std::getline(line, record, delimiter)) 
+            int counter{0};
+            while(std::getline(sstream, record)) 
                 {
-                    items.push_back(record);
+                    std::istringstream line(record);
+                    while(std::getline(line, record, delimiter)) 
+                    {
+                        items.push_back(record);
+                    }
+                    csv_contents[counter] = items;
+                    items.clear();
+                    counter += 1;
                 }
-                csv_contents[counter] = items;
-                items.clear();
-                counter += 1;
+
+            return csv_contents;
+        }
+
+        void getCSVContents()
+        {
+            csv_contents = getFileContent();
+        }
+
+        void getWheelRadius()
+        {
+        wheelRadius = std::stod (csv_contents[1][0], nullptr); 
+        }
+
+        void getWheelToCenter()
+        {
+            wheelToCenter = std::stod(csv_contents[1][1], nullptr);
+        }
+
+        void getOmega1()
+        {
+            for(int i{3}; i < csv_contents.size(); i++)
+            {
+                omega1.push_back(std::stod(csv_contents[i][1]));
             }
-
-        return csv_contents;
-    }
-
-    void getCSVContents()
-    {
-        csv_contents = getFileContent();
-    }
-
-    void getWheelRadius()
-    {
-       wheelRadius = std::stod (csv_contents[1][0], nullptr); 
-    }
-
-    void getWheelToCenter()
-    {
-        wheelToCenter = std::stod(csv_contents[1][1], nullptr);
-    }
-
-    void getOmega1()
-    {
-        for(int i{3}; i < csv_contents.size(); i++)
-        {
-            omega1.push_back(std::stod(csv_contents[i][1]));
         }
-    }
 
-    void getOmega2()
-    {
-        for(int i{3}; i < csv_contents.size(); i++)
+        void getOmega2()
         {
-            omega2.push_back(std::stod(csv_contents[i][2]));
+            for(int i{3}; i < csv_contents.size(); i++)
+            {
+                omega2.push_back(std::stod(csv_contents[i][2]));
+            }
         }
-    }
 
-    void getTime()
-    {
-        for(int i{3}; i < csv_contents.size(); i++)
+        void getTime()
         {
-            time.push_back(std::stod(csv_contents[i][0]));
+            for(int i{3}; i < csv_contents.size(); i++)
+            {
+                time.push_back(std::stod(csv_contents[i][0]));
+            }
         }
-    }
 };
