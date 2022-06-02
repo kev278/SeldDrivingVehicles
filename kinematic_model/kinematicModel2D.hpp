144544c34b569c9df99af93b75e2ac791f84338b
Keval Shah <kevalshah278@gmail.com>
2022-06-02 11:39:23 -0400

    added 2 more variables to parse

--- kinematic_model/kinematicModel2D.hpp
+++ kinematic_model/kinematicModel2D.hpp
@@ -5,30 +5,25 @@ A simple Kinematic Model of a 2-Wheeled 2D Robot
 #include "inputData.hpp"
 #include <cmath>
 
-class Robot
+class Vehicle
 {
-    Input Data;
-    double wheelRadius = Data.getWheelRadius();
-    double wheelToCenter = Data.getWheelToCenter();
-    std::vector<double> omega1 = Data.getOmega1();
-    std::vector<double> omega2 = Data.getOmega2();
-    std::vector<double> time = Data.getTime();
-
     public:
-        double calcX(double x_prev, double theta_prev, double omega1, double omega2, double deltaT)
+
+        double calcX(double x_prev, double theta_prev, double omega1, double omega2, double deltaT, double wheelRadius)
         {
-            double x_curr = x_prev + ((wheelRadius * (omega1 + omega2) / 2 * cos(theta_prev))) * deltaT;
+            double x_curr = x_prev + (wheelRadius * (omega1 + omega2) / 2 * cos(theta_prev)) * deltaT;
             return x_curr;
         }
 
-        double calcY(double y_prev, double theta_prev, double omega1, double omega2, double deltaT)
+        double calcY(double y_prev, double theta_prev, double omega1, double omega2, double deltaT, double wheelRadius)
         {
             double y_curr = y_prev + (wheelRadius * (omega1 + omega2) / 2 * sin(theta_prev)) * deltaT;
             return y_curr;
         }
 
-        double calcTheta(double theta_prev, double omega1, double omega2, double deltaT)
+        double calcTheta(double theta_prev, double omega1, double omega2, double deltaT, double wheelRadius, double wheelToCenter)
         {
             double theta_curr = theta_prev + (wheelRadius * (omega1 - omega2) / (2 * wheelToCenter)) * deltaT;
+            return theta_curr;
         }
 };
